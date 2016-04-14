1392fd05cab8c312758ed83fd3377f54735d9ee0
Lars De Richter <lars@anansi.be>
2016-04-14 14:52:06 +0200

    and now it becomes smaller as well as larger

--- scripts/wn.js
+++ scripts/wn.js
@@ -4,5 +4,7 @@
 $(window).scroll(function() {
     if ($(window).scrollTop() > 2 && !($("#page-header").hasClass("wn-header--collapsed"))) {
         $("#page-header").addClass("wn-header--collapsed");
+    } else if (($(window).scrollTop() < 2 && $("#page-header").hasClass("wn-header--collapsed"))) {
+        $("#page-header").removeClass("wn-header--collapsed");
     }
 });
