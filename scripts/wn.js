7e0bc48d16817aea1341790d4da9551971145a8f
Lars De Richter <lars@anansi.be>
2016-04-19 15:55:22 +0200

    Got scrollto working

--- scripts/wn.js
+++ scripts/wn.js
@@ -1,7 +1,6 @@
 /**
  * Making the cards of equal height
  */
-
 $(".card").matchHeight();
 
 
@@ -14,4 +13,15 @@ $(window).scroll(function() {
     } else if (($(window).scrollTop() < 2 && $("#page-header").hasClass("wn-header--collapsed"))) {
         $("#page-header").removeClass("wn-header--collapsed");
     }
+});
+
+/**
+ * Scrollto
+ **/
+$(".wn-nav-link").click(function() {
+    var offset = $($.attr(this, "href")).offset().top - 180;
+    $("html, body").animate({
+        scrollTop: offset
+    }, 2000);
+    return false;
 });
