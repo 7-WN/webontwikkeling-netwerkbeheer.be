942d771a612e6d926bb84581aa729cf70af42308
Lars De Richter <lars.derichter@gmail.com>
2017-03-06 11:57:23 +0100

    A whole lot that was never commited

---
+++ scripts/wn.js
@@ -0,0 +1,38 @@
+/**
+ * Making the cards of equal height
+ */
+$(".card").matchHeight();
+
+
+/**
+ * Resizing of the page header
+ **/
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 2 && !($("#page-header").hasClass("wn-header--collapsed"))) {
+        $("#page-header").addClass("wn-header--collapsed");
+    } else if (($(window).scrollTop() < 2 && $("#page-header").hasClass("wn-header--collapsed"))) {
+        $("#page-header").removeClass("wn-header--collapsed");
+    }
+});
+
+/**
+ * Scrollto
+ **/
+$(".wn-nav-link").click(function() {
+    var offset = $($.attr(this, "href")).offset().top - 180;
+    $("html, body").animate({
+        scrollTop: offset
+    }, 2000);
+    return false;
+});
+
+/**
+ * Navigation Scrollspy
+ */
+$('.scrollspy').scrollSpy();
+
+
+/**
+ * Toast message
+ */
+Materialize.toast('Kom naar ons infomoment op 30 juni van 13.00 tot 16.00', 5000, "wn-info-toast");
