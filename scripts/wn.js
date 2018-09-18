b2dd9b1f9fde42e5d968ca8825a29ca13d33a248
Lars De Richter <lars.derichter@gmail.com>
2018-09-18 15:19:33 +0200

    Update inschrijvingsboodschap.

--- scripts/wn.js
+++ scripts/wn.js
@@ -3,14 +3,19 @@
  */
 $(".card").matchHeight();
 
-
 /**
  * Resizing of the page header
  **/
 $(window).scroll(function() {
-  if ($(window).scrollTop() > 2 && !($("#page-header").hasClass("wn-header--collapsed"))) {
+  if (
+    $(window).scrollTop() > 2 &&
+    !$("#page-header").hasClass("wn-header--collapsed")
+  ) {
     $("#page-header").addClass("wn-header--collapsed");
-  } else if (($(window).scrollTop() < 2 && $("#page-header").hasClass("wn-header--collapsed"))) {
+  } else if (
+    $(window).scrollTop() < 2 &&
+    $("#page-header").hasClass("wn-header--collapsed")
+  ) {
     $("#page-header").removeClass("wn-header--collapsed");
   }
 });
@@ -20,19 +25,25 @@ $(window).scroll(function() {
  **/
 $(".wn-nav-link").click(function() {
   var offset = $($.attr(this, "href")).offset().top - 180;
-  $("html, body").animate({
-    scrollTop: offset
-  }, 2000);
+  $("html, body").animate(
+    {
+      scrollTop: offset
+    },
+    2000
+  );
   return false;
 });
 
 /**
  * Navigation Scrollspy
  */
-$('.scrollspy').scrollSpy();
-
+$(".scrollspy").scrollSpy();
 
 /**
  * Toast message
  */
-Materialize.toast('Je kan nog inschrijven tot 6 juli, daarna terug vanaf 22 augustus.', 5000, "wn-info-toast");
+Materialize.toast(
+  "Er is nog plaats! Je kan nog zeker de hele maand september mee inschuiven.",
+  5000,
+  "wn-info-toast"
+);
